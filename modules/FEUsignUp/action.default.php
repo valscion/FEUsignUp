76c31209072c45bd42448cbe4f5e38f18466ac72
VesQ <laakso.vesa@gmail.com>
2011-04-09 15:20:58 +0300

    Jotain tapahtumaan jo kun lisätään tagi sivulle.

--- modules/FEUsignUp/action.default.php
+++ modules/FEUsignUp/action.default.php
@@ -12,6 +12,31 @@ if (!isset($gCms)) exit;
    
 */
 
-echo cgcalendar_utils::get_username(1);
+$feu =& cge_utils::get_module('FrontEndUsers');
+if( $feu === null ) die('Front End Users module is not installed!');
+
+$cgcal =& cge_utils::get_module('CGCalendar');
+if( $cgcal === null ) die('CGCalendar module is not installed!');
+
+if( !$feu->LoggedIn() ) { 
+    echo('You\'re not logged in!'); 
+    //return; 
+} else {
+    echo $feu->LoggedInName();
+}
+
+echo '<pre>';
+if( isset($params['cal_id']) ) {
+    $event = $cgcal->GetEvent((int) $params['cal_id']);
+    print_r( $event );
+    echo "\n--------\n";
+    foreach( $event['categories'] as $catId ) {
+        echo $catId . ": ";
+        echo $cgcal->GetCategoryName( 2 ) . "\n";
+    }
+} else {
+    print_r( $cgcal->GetCategories() );
+}
+echo '</pre>';
 
 ?>
