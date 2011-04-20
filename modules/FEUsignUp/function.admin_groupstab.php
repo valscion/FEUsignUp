ecff42d53664813ff7c1ceb6edc44a00da78c439
VesQ <laakso.vesa@gmail.com>
2011-04-20 17:50:00 +0300

    Admin-puolen Groups-tabiin tulostukset feu-ryhmistä ja tss-joukkueista

--- modules/FEUsignUp/function.admin_groupstab.php
+++ modules/FEUsignUp/function.admin_groupstab.php
@@ -9,4 +9,53 @@ if (!isset($gCms)) exit;
    
 */
 
-echo 'Hello world! Here\'s the "groups" tab ;)';
+$feu =& cge_utils::get_module('FrontEndUsers');
+if( $feu === null ) die('Front End Users module is not installed!');
+
+$cgcal =& cge_utils::get_module('CGCalendar');
+if( $cgcal === null ) die('CGCalendar module is not installed!');
+
+$tss =& cge_utils::get_module('TeamSportScores');
+if( $tss === null ) die('TeamSportScores module is not installed!');
+
+// Let's hack the DB directly to get all the possible information with only one query.
+$db =& $this->GetDb(); /* @var $db ADOConnection */
+
+// Assign FrontEndUsers groups to $feu_groups
+$feu_groups = array();
+$q = "SELECT id,groupname,groupdesc FROM ".cms_db_prefix()."module_feusers_groups";
+$dbresult = $db->Execute( $q );
+if( $dbresult ) {
+    while( $row = $dbresult->FetchRow() ) {
+        $feu_groups[] = array(
+            'group_id' => $row['id'],
+            'group_name' => $row['groupname'],
+            'group_desc' => $row['groupdesc']
+        );
+    }
+}
+
+// Assign TSS teams to $tss_teams
+$tss_teams = array();
+// Ignore the NONE-team.
+$q = "SELECT team_id,team_name,team_code FROM ".cms_db_prefix()."module_tss_team WHERE team_id != 0";
+$dbresult = $db->Execute( $q );
+if( $dbresult ) {
+    while( $row = $dbresult->FetchRow() ) {
+        $tss_teams[] = array(
+            'team_id' => $row['team_id'],
+            'team_name' => $row['team_name'],
+            'team_code' => $row['team_code']
+        );
+    }
+}
+
+// Assign fetched groups and teams to smarty
+$this->smarty->assign('feu_groups',$feu_groups);
+$this->smarty->assign('tss_teams',$tss_teams);
+
+// Run the template
+
+echo $this->ProcessTemplate('admin_groupstab.tpl');
+
+?>
---
+++ modules/FEUsignUp/templates/admin_groupstab.tpl
@@ -0,0 +1,6 @@
+Hello world! This is groups-tab.
+<pre>{$feu_groups|print_r}
+
+----
+
+{$tss_teams|print_r}</pre>
