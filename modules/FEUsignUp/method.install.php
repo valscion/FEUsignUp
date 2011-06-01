5e4995ca170ffce8a6d9672c77b4de03d97bf39a
VesQ <laakso.vesa@gmail.com>
2011-06-02 01:38:53 +0300

    Turhat välilyönnit alusta pois installista ja uninstallista

--- modules/FEUsignUp/method.install.php
+++ modules/FEUsignUp/method.install.php
@@ -1,72 +1,72 @@
 <?php
 if (!isset($gCms)) exit;
 
-    /*---------------------------------------------------------
-       Install()
-       When your module is installed, you may need to do some
-       setup. Typical things that happen here are the creation
-       and prepopulation of database tables, database sequences,
-       permissions, preferences, etc.
-       
-       For information on the creation of database tables,
-       check out the ADODB Data Dictionary page at
-       http://phplens.com/lens/adodb/docs-datadict.htm
-       
-       This function can return a string in case of any error,
-       and CMS will not consider the module installed.
-       Successful installs should return FALSE or nothing at all.
-      ---------------------------------------------------------*/
-        
-        // Typical Database Initialization
-        $db =& $gCms->GetDb();
-        
-        // mysql-specific, but ignored by other database
-        $taboptarray = array('mysql' => 'TYPE=MyISAM');
-        $dict = NewDataDictionary($db);
-        
-        // table schema description
-        $flds = "
-            id I KEY,
-            feu_user_id I NOTNULL,
-            cgcal_event_id I DEFAULT -1,
-            tss_game_id I DEFAULT -1,
-            group_id I DEFAULT -1,
-            signed_up L DEFAULT 0,
-            description C(255)
-            ";
+/*---------------------------------------------------------
+   Install()
+   When your module is installed, you may need to do some
+   setup. Typical things that happen here are the creation
+   and prepopulation of database tables, database sequences,
+   permissions, preferences, etc.
+   
+   For information on the creation of database tables,
+   check out the ADODB Data Dictionary page at
+   http://phplens.com/lens/adodb/docs-datadict.htm
+   
+   This function can return a string in case of any error,
+   and CMS will not consider the module installed.
+   Successful installs should return FALSE or nothing at all.
+  ---------------------------------------------------------*/
 
-        // create it. This should do error checking, but I'm a lazy sod.
-        $sqlarray = $dict->CreateTableSQL($this->events_table_name,
-                $flds, $taboptarray);
-        $dict->ExecuteSQLArray($sqlarray);
+// Typical Database Initialization
+$db =& $gCms->GetDb();
 
-        // create a sequence
-        $db->CreateSequence($this->events_table_name.'_seq');
-        
-        
-        // table schema description
-        $flds = "
-            linking_id I KEY,
-            feusers_group_id I NOTNULL,
-            cgcal_category_id I DEFAULT -1,
-            tss_team_id I DEFAULT -1,
-            description C(255)
-            ";
+// mysql-specific, but ignored by other database
+$taboptarray = array('mysql' => 'TYPE=MyISAM');
+$dict = NewDataDictionary($db);
 
-        // create it. This should do error checking, but I'm a lazy sod.
-        $sqlarray = $dict->CreateTableSQL($this->linkings_table_name,
-                $flds, $taboptarray);
-        $dict->ExecuteSQLArray($sqlarray);
+// table schema description
+$flds = "
+    id I KEY,
+    feu_user_id I NOTNULL,
+    cgcal_event_id I DEFAULT -1,
+    tss_game_id I DEFAULT -1,
+    group_id I DEFAULT -1,
+    signed_up L DEFAULT 0,
+    description C(255)
+    ";
 
-        // create a sequence
-        $db->CreateSequence($this->linkings_table_name.'_seq');
-        
-        
-        // permissions
-        $this->CreatePermission('Use FEUsignUp', 'Use FEUsignUp');
-        
+// create it. This should do error checking, but I'm a lazy sod.
+$sqlarray = $dict->CreateTableSQL($this->events_table_name,
+        $flds, $taboptarray);
+$dict->ExecuteSQLArray($sqlarray);
 
-        // put mention into the admin log
-        $this->Audit( 0, $this->Lang('friendlyname'), $this->Lang('installed',$this->GetVersion()));
+// create a sequence
+$db->CreateSequence($this->events_table_name.'_seq');
+
+
+// table schema description
+$flds = "
+    linking_id I KEY,
+    feusers_group_id I NOTNULL,
+    cgcal_category_id I DEFAULT -1,
+    tss_team_id I DEFAULT -1,
+    description C(255)
+    ";
+
+// create it. This should do error checking, but I'm a lazy sod.
+$sqlarray = $dict->CreateTableSQL($this->linkings_table_name,
+        $flds, $taboptarray);
+$dict->ExecuteSQLArray($sqlarray);
+
+// create a sequence
+$db->CreateSequence($this->linkings_table_name.'_seq');
+
+
+// permissions
+$this->CreatePermission('Use FEUsignUp', 'Use FEUsignUp');
+
+
+// put mention into the admin log
+$this->Audit( 0, $this->Lang('friendlyname'), $this->Lang('installed',$this->GetVersion()));
         
 ?>
--- modules/FEUsignUp/method.uninstall.php
+++ modules/FEUsignUp/method.uninstall.php
@@ -1,43 +1,43 @@
 <?php
 if (!isset($gCms)) exit;
 
-    /*---------------------------------------------------------
-       Uninstall()
-       Sometimes, an exceptionally unenlightened or ignorant
-       admin will wish to uninstall your module. While it
-       would be best to lay into these idiots with a cluestick,
-       we will do the magnanimous thing and remove the module
-       and clean up the database, permissions, and preferences
-       that are specific to it.
-       This is the method where we do this.
-      ---------------------------------------------------------*/
+/*---------------------------------------------------------
+ Uninstall()
+ Sometimes, an exceptionally unenlightened or ignorant
+ admin will wish to uninstall your module. While it
+ would be best to lay into these idiots with a cluestick,
+ we will do the magnanimous thing and remove the module
+ and clean up the database, permissions, and preferences
+ that are specific to it.
+ This is the method where we do this.
+ ---------------------------------------------------------*/
 
-        
-        // Typical Database Removal
-        $db =& $gCms->GetDb();
-        
-        // remove the database table
-        $dict = NewDataDictionary( $db );
-        $sqlarray = $dict->DropTableSQL( $this->events_table_name );
-        $dict->ExecuteSQLArray($sqlarray);
 
-        // remove the sequence
-        $db->DropSequence( $this->events_table_name.'_seq' );
-        
-        // remove the database table
-        $dict = NewDataDictionary( $db );
-        $sqlarray = $dict->DropTableSQL( $this->linkings_table_name );
-        $dict->ExecuteSQLArray($sqlarray);
+// Typical Database Removal
+$db =& $gCms->GetDb();
 
-        // remove the sequence
-        $db->DropSequence( $this->linkings_table_name.'_seq' );
-        
-        
-        // remove the permissions
-        $this->RemovePermission('Use FEUsignUp');
-        
-        
-        // put mention into the admin log
-        $this->Audit( 0, $this->Lang('friendlyname'), $this->Lang('uninstalled'));
+// remove the database table
+$dict = NewDataDictionary( $db );
+$sqlarray = $dict->DropTableSQL( $this->events_table_name );
+$dict->ExecuteSQLArray($sqlarray);
+
+// remove the sequence
+$db->DropSequence( $this->events_table_name.'_seq' );
+
+// remove the database table
+$dict = NewDataDictionary( $db );
+$sqlarray = $dict->DropTableSQL( $this->linkings_table_name );
+$dict->ExecuteSQLArray($sqlarray);
+
+// remove the sequence
+$db->DropSequence( $this->linkings_table_name.'_seq' );
+
+
+// remove the permissions
+$this->RemovePermission('Use FEUsignUp');
+
+
+// put mention into the admin log
+$this->Audit( 0, $this->Lang('friendlyname'), $this->Lang('uninstalled'));
 
 ?>
